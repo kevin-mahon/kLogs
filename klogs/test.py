51411515f29d8eb559e5032fe2e65c74310e6c4c
kevin-mahon <k.mahon.dev@gmail.com>
2025-06-19 12:22:14 -0400

    Added process tests, fixed tests for file logging to add info between tests in logfile

--- klogs/test.py
+++ klogs/test.py
@@ -1,12 +1,11 @@
 import logging
 import argparse
 import threading
+import multiprocessing as mp
 from kLogger import kLogger
 
-def test(logfile, loglevel):
-    #test creation
-    log = kLogger("klogs", logfile, loglevel)
 
+def basic_test(log):
     #test default usage
     log.debug("debug message")
     log.info("info message")
@@ -19,27 +18,71 @@ def test(logfile, loglevel):
     x = 10
     log(x)
 
+def test(logfile, loglevel):
+    #test creation
+    log = kLogger("klogs", logfile, loglevel)
+    basic_test(log)
+
 def test_threads(logfile, loglevel):
     log = kLogger("klogs", logfile, loglevel)
     
     def thread():
-        log.debug("debug message")
-        log.info("info message")
-        log.warning("warning message")
-        log.error("error message")
-        log.critical("critical message")
+        basic_test(log)
 
-        #test calls
-        log()
-        x = 10
-        log(x)
-
-    print("######## In thread ########")
+    info_string = "######## In thread ########"
+    if logfile:
+        with open(logfile, "a") as fd:
+            fd.write(info_string)
+            fd.write("\n")
+    else:
+        print(info_string)
     t = threading.Thread(target=thread)
     t.start()
     t.join()
-    print("######## Outside thread ########")
-    thread()
+    info_string = "######## Outside thread ########"
+    if logfile:
+        with open(logfile, "a") as fd:
+            fd.write(info_string)
+            fd.write("\n")
+    else:
+        print(info_string)
+    basic_test(log)
+
+
+def process(logfile, loglevel):
+    log = kLogger("klogs", logfile, loglevel)
+    log.debug("debug message")
+    log.info("info message")
+    log.warning("warning message")
+    log.error("error message")
+    log.critical("critical message")
+
+    #test calls
+    log()
+    x = 10
+    log(x)
+
+def test_process(logfile, loglevel):
+    log = kLogger("klogs", logfile, loglevel)
+    info_string = "######## In process ########"
+    if logfile:
+        with open(logfile, "a") as fd:
+            fd.write(info_string)
+            fd.write("\n")
+    else:
+        print(info_string)
+    p = mp.Process(target=process, args=(logfile, loglevel,))
+    p.start()
+    p.join()
+    info_string = "######## Outside process ########"
+    if logfile:
+        with open(logfile, "a") as fd:
+            fd.write(info_string)
+            fd.write("\n")
+    else:
+        print(info_string)
+    basic_test(log)
+    
 
 if __name__ == "__main__":
     #argparsing 
@@ -47,8 +90,28 @@ if __name__ == "__main__":
     argparser.add_argument('-f', '--file', help='Log file')
     argparser.add_argument('-l', '--level', help='Log level')
     args = argparser.parse_args()
-    print("######## Testing normal usage ########")
+    info_string = "######## Testing normal usage ########"
+    if args.file:
+        with open(args.file, "w") as fd:
+            fd.write(info_string)
+            fd.write("\n")
+    else:
+        print(info_string)
     test(args.file, args.level)
-    print("######## Testing threaded usage ########")
+    info_string = "######## Testing threaded usage ########"
+    if args.file:
+        with open(args.file, "a") as fd:
+            fd.write(info_string)
+            fd.write("\n")
+    else:
+        print(info_string)
     test_threads(args.file, args.level)
+    info_string = "######## Testing process usage ########"
+    if args.file:
+        with open(args.file, "a") as fd:
+            fd.write(info_string)
+            fd.write("\n")
+    else:
+        print(info_string)
+    test_process(args.file, args.level)
 
